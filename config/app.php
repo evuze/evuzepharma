34aa30e35c738f1c3bc897160b89d09efa90b0c8
Hirwa Felix <hirwa.felix@gmail.com>
2017-06-29 02:43:20 +0200

    Adding 'Voyager The Missing Admin' Package

--- config/app.php
+++ config/app.php
@@ -166,7 +166,9 @@ return [
         /*
          * Package Service Providers...
          */
+
         Laravel\Tinker\TinkerServiceProvider::class,
+	TCG\Voyager\VoyagerServiceProvider::class,
 
         /*
          * Application Service Providers...
