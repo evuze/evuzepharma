66f28c8b6054d1298dabb22251e3d6041b7e5caa
Mucyo F Miller <mucyomiller@gmail.com>
2017-08-30 23:12:20 +0200

    add sales,clients & insurance models

---
+++ database/migrations/2017_08_30_202828_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('particulars');
+            $table->string('vch_type'); //Voucher Type
+            $table->string('vch_no'); // Voucher No (incremental)
+            $table->string('amount_dbt'); //amount debit
+            $table->string('amount_cdt');// amount credit
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
